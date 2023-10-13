2d12cb8711083bcd23ccaa98a4cfc3a3e7f12e68
Carlos Abaunza <cabaunza@unab.edu.co>
2023-10-12 22:32:55 -0500

    Activities Login and Register

--- app/src/main/java/com/fabiansuarez/mywallet/LoginActivity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/LoginActivity.java
@@ -2,13 +2,29 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnGoToRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        asociateViewXML();
+
+        btnGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+    private void asociateViewXML() {
+        btnGoToRegister = findViewById(R.id.btn_go_to_register);
     }
 }
--- app/src/main/java/com/fabiansuarez/mywallet/RegisterActivity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/RegisterActivity.java
@@ -2,13 +2,30 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private ImageView iGoToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        asociateViewXML();
+
+        iGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+    private void asociateViewXML() {
+        iGoToLogin = findViewById(R.id.imageView);
     }
 }
